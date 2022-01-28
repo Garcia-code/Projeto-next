df2c346572c2399aabf74d9fc5f8192c9c4f9132
Garcia-code <61274532+Garcia-code@users.noreply.github.com>
2022-01-28 13:23:40 -0300

    Add files via upload

--- AuthContext.js
+++ AuthContext.js
@@ -1,4 +1,7 @@
 import { createContext, useState } from "react";
+import firebase from "../lib/firebase";
+import Router from "next/router";
+import { signInWithPopup } from "firebase/auth";
 
 const AuthContext = createContext();
 
@@ -6,10 +9,45 @@ export const AuthProvider ; ({ children }) => {
     const [user,setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
-    const signin = () => {}
-    const signout = () => {}
+    const signin = () => {
+        try{
+            setLoading(true);
+            return firebase
+            auth()
+            signInWithPopup (new firebase.auth.GithubAuthProvider())
+            then ((response)); {
+                setUser(response.user);
+                Router.push('/Dashboard');
 
-    return
+            }
+
+        } finally {
+            setLoading(false);
+        
+        }
+    
+    }
+        
+    const signout = () => {
+        try {
+            Router.push('/');
+
+            return firebase 
+            auth()
+            signOut()
+            then(() => setUser(false));
+
+        } finally{
+            setLoading(false);
+        }
+    }
+
+    return <AuthContext.Provider value={{
+        user,
+        loading,
+        signin,
+        signout
+    }} >{children}</AuthContext.Provider>
 
 } 
    
